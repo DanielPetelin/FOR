db269a394c66caacb14adde6cd3d7ab416ce259a
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-16 16:28:27 +0300

    number 3

--- js/script.js
+++ js/script.js
@@ -3,8 +3,14 @@
     //console.log(i);
 //}
 
+//Задача 2
 //for (var i = 4; i <= 16; i++) {
     //if (i % 3 == 1) {
         //console.log(i);
     //}
 //}
+
+//Задача 3
+for (var i = 654; i >= 650; i--) {
+    console.log(i);
+}
